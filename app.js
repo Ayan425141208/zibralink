fdecbdc62129d54f4fbe79b56b563c604c0ddb34
Ayan425141208 <66969596+Ayan425141208@users.noreply.github.com>
2020-07-31 20:34:14 +0600

    first web service
    Zibralink official app development

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 5000));
+app.get('/infozibralink', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public','info.html'));
+})
+
+app.get('/regform', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'App.html'))
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'Form.html'))
+})
+
+app.get('/contact', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public','contact.html'));
+})
+
+app.get('/home', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public','Home.html'));
+})
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public','front.html'));
+})
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+})
